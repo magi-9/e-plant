d62acf0d432ea1df0e18261ffbff74eb2e8d4951
Tomas Magula <magula12@uniba.sk>
2026-02-10 21:34:28 +0200

    Commit 3: Django base project: create project config, requirements.txt, settings split (base/dev)

---
+++ backend/config/settings/dev.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'e_plant_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
