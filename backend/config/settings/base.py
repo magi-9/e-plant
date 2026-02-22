56aadf7314719088e4896769196694cd22485fa9
Tomas Magula <magula12@uniba.sk>
2026-02-22 13:29:19 +0200

    chore: change ports to 5001 5002 5003 across docker, frontend, and backend settings

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -63,8 +63,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://localhost:5001",
+    "http://127.0.0.1:5001",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
